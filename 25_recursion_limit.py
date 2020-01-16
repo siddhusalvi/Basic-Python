f2eac93c0e2032c3333147bbac87262991cca9d2
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 17:00:46 +0530

    Refactor : final check

--- 25_recursion_limit.py
+++ 25_recursion_limit.py
@@ -2,4 +2,4 @@
 25. Write a Python program to get the current value of the recursion limit.
 """
 import sys
-print("Current value of the recursion limit:",sys.getrecursionlimit())
+print("Current value of the recursion limit:", sys.getrecursionlimit())
