50d54c37803510257b7aadab0b9a5d8457bb0096
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 15:43:38 +0530

    Refactor:variable updates

--- 1_firstNameLastName.py
+++ 1_firstNameLastName.py
@@ -1,15 +1,15 @@
 """1. Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them. 
  """
 
-def reverse(s): 
-  str = "" 
-  for i in s: 
-    str = i + str
-  return str
+def reverse(word):
+  new_word = ""
+  for i in word:
+    new_word = i + new_word
+  return new_word
 
 fname = input("Input your First Name : ")
 lname = input("Input your Last Name : ")
 fname = reverse(fname)
 lname = reverse(lname)
-print ("Hello  " + lname + " " + fname)
+print ("Hello  " + fname + " " + lname)
 
