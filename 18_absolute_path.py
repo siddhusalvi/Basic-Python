96d16e54c000cfc9e1dac07a8fa073a16ffc8507
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 16:51:18 +0530

    Refactor : final check

--- 18_absolute_path.py
+++ 18_absolute_path.py
@@ -1,7 +1,9 @@
 import os
-def get_absolute_path(fname):
-        return os.path.abspath(fname)
 
 
-file = "runtime.py"    
-print("Absolute path of : ",file,"is",get_absolute_path(file))
+def get_absolute_path(filename):
+    return os.path.abspath(filename)
+
+
+file = "runtime.py"
+print("Absolute path of : ", file, "is", get_absolute_path(file))
