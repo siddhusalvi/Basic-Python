641957c0f7f257cbcc3aabd3d797e3e0dfd69602
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 16:28:41 +0530

    Refactor : final check

--- 9_concatenate.py
+++ 9_concatenate.py
@@ -1,13 +1,16 @@
 """
 9. Write a Python program to concatenate all elements in a list into a string and return it.  
 """
-def concatenate_list(list): #Function to concatenate list
-    output =""
-    for data in list:
-        output += str(data)
+
+
+def concatenate_list(list1):  # Function to concatenate list
+    output = ""
+    for i in list1:
+        output += str(i)
     return output
 
-data = ['a','b','c','d','e']
+
+data = ['a', 'b', 'c', 'd', 'e']
 
 string = concatenate_list(data)
 print(string)
