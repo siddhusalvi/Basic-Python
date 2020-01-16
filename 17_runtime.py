00942cc92b2643f11deef954de442a631dd4cdf2
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 16:49:30 +0530

    Refactor : final check

--- 17_runtime.py
+++ 17_runtime.py
@@ -3,10 +3,11 @@ Write a program to get execution time for a Python method.
 """
 
 import time
+
 start = time.time()
 
-for i in range(100000000):#approx 5 sec
-   num = i 
+for i in range(100000000): 
+    number = i
 end = time.time()
-time = end-start
+time = end - start
 print(time)
