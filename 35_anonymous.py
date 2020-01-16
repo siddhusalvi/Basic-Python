6373bba990fbfa8d1b825be9c9bc3fc8f6256820
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 17:07:51 +0530

    Refactor : final check

--- 35_anonymous.py
+++ 35_anonymous.py
@@ -4,8 +4,7 @@ function.
 """
 
 numbers = [47,78,12,15,64,19,30,300]
-#result = list(filter(lambda x: (x % 15 == 0), numbers))
 result = []
 for i in numbers:
-    lambda i : (i % 15 == 0),result.append(i)
-print(result)
+    lambda j: (i % 15 == 0), result.append(i)
+print(result) 
