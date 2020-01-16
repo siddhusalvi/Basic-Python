a9cb337d4e5064a747d269b4f3e9db176d0b15d1
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 17:03:19 +0530

    Refactor : final check

--- 26_count_occurance.py
+++ 26_count_occurance.py
@@ -1,12 +1,16 @@
 """
 26. Write a Python program to count the number occurrence of a specific character in a string. 
 """
-def count_occurance(word,charc):#function to return count
-    count = 0;
+
+
+def count_occur(word, c):  # function to return count
+    count = 0
     for i in range(len(word)):
-        if(word[i] == c):
+        if word[i] == c:
             count += 1
     return count
-str= "thisisveryverybigword"
+
+
+word = "thisisveryverybigword"
 c = 'i'
-print(count_occurance(str, c)) 
+print(count_occur(word, c))
