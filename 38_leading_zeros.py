2a8dae37d806b5c00843bb38da4ba1ca11867dd9
Siddhesh salvi <siddheshsalvi525@gmail.com>
2020-01-16 17:13:00 +0530

    Refactor : final check

--- 38_leading_zeros.py
+++ 38_leading_zeros.py
@@ -2,11 +2,15 @@
 38. Write a Python program to add leading zeroes to a string.
 Python string method ljust() returns the string left justified in a string of length 
 """
-def add_leadin_zeros(str,num):
+
+
+def add_leading_zeros(word, number):
     zeros = ""
-    for x in range(num):
+    for x in range(number):
         zeros += "0"
-    return zeros + str
-str1='1.78'
-num =4
-print(add_leadin_zeros(str1,num))
+    return zeros + word
+
+
+str1 = '1.78'
+num = 4
+print(add_leading_zeros(str1, num))
